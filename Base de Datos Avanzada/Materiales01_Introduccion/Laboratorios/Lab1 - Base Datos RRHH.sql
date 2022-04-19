03219eb30e46000ce296c1b3f46ad4720acce20e
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-04-19 16:32:38 -0500

    fe

--- Base de Datos Avanzada/Materiales01_Introduccion/Laboratorios/Lab1 - Base Datos RRHH.sql
+++ Base de Datos Avanzada/Materiales01_Introduccion/Laboratorios/Lab1 - Base Datos RRHH.sql
@@ -20,16 +20,15 @@ CREATE TABLE Empleado (
 GO
 -- Hacemos algunas modificaciones a la tabla Empleado
 -- Agregaremos los campos DNI  Sueldo y email, pues los agregamos con ALTER TABLE
-  ALTER TABLE EMPLEADO
-        ADD DNIEmpleado NCHAR(8) not null , 
-		   emailEmpleado NVARCHAR(30), 
-		   sueEmpleado Money
-  GO
+ALTER TABLE EMPLEADO
+	ADD emailEmpleado NVARCHAR(30), 
+	sueEmpleado Money
+GO
 
   -- Removeremos el campo NroPoliza (dado que en nuestro ejemplo no hace falta en la tabla) 
-  ALTER TABLE EMPLEADO
-         DROP COLUMN NroPolizaEmpleado 
-  GO
+ALTER TABLE EMPLEADO
+	DROP COLUMN NroPolizaEmpleado 
+GO
    
   -- Otros casos que se pueden dar, por ejemplo cambiar el tamaño de un campo
   -- El nombre de sucursal de 35 caracteres a 50 caracteres.
@@ -53,22 +52,36 @@ SELECT * FROM Departamento;
 --  Definamos la FK en la tabla Empleado apuntando a la tabla Departamento
 --  en base al campo codDepartamento 
 
-
+alter table EMPLEADO
+	ADD FOREIGN KEY (codDepartamento) REFERENCES Departamento
+go
 -- Definamos los siguentes constraint
 
 -- 1. El sueldo del empleado debe estar en un rango de 950 a 4500 soles.
+ALTER TABLE EMPLEADO
+	ADD CONSTRAINT chk_sueEmpleado CHECK
+	(sueEmpleado between 950 and 4500)
 
 -- 2. El DNI del empleado debe ser de 8 digitos.
+ALTER TABLE Empleado 	ADD CONSTRAINT chk_DNIEmpleado CHECK 	(DNIEmpleado LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')go
 
 -- 3. En el caso de que al momento de insertar un empleado se omita la fecha de ingreso, se asuma por defecto 
 -- que la fecha de ingreso sea la fecha actual
-
+ALTER TABLE EMPLEADO
+	ADD CONSTRAINT Def_fecIngEmpleado DEFAULT
+	getdate() FOR fecIngEmpleado
+go
 
 -- 4. El campo DNIEmpleado de la tabla Empleado debe ser unico.
-
+ALTER TABLE EMPLEADO 
+	ADD CONSTRAINT QQ_DNIEmpleado UNIQUE (DNIEmpleado)
+GO
 
 --Agregamos 4 empleados y validamos los constraints 
 -- Se emplea un Insert para los 3 registros 
+SELECT * FROM Empleado
+go
+
 
 INSERT INTO EMPLEADO 
 	VALUES ( 'Jose', 'Leon','19660226','20150320',1,'08863451','pjleon@yahoo.com',2900),
@@ -76,3 +89,7 @@ INSERT INTO EMPLEADO
 	       ( 'Eduardo', 'Cruzado','19800726',NULL,1,'02363455','ecruzado@live.com',5000)
 go
 
+INSERT INTO EMPLEADO
+(Nombres,Apellidos,fechaNacimiento,codDepartamento,DNIEmpleado)
+VALUES ('Marco','Salas','19990104',2,'09965123')
+go
--- Base de Datos Avanzada/Materiales01_Introduccion/Laboratorios/Lab3 - Constraints.sql
+++ Base de Datos Avanzada/Materiales01_Introduccion/Laboratorios/Lab3 - Constraints.sql
@@ -20,31 +20,53 @@ go
 
 -- 1. Que el campo Tip_Ven de la tabla Tb_Vendedor solo almacene los valores '1' o '2'
 
-
+ALTER TABLE Tb_Vendedor
+	ADD CONSTRAINT chk_Top_Ven CHECK
+	(Tip_Ven in (1,2))
+GO
 -- 2. Que el campo Est_Fac de la tabla Tb_Factura solo permita
 -- los valores 1 (Pendiente), 2(Cancelada o Pagada) o 3 (Anulada)
-
+ALTER TABLE Tb_Factura
+	ADD CONSTRAINT chk_Est_Fac CHECK
+	(Est_Fac in (1,2,3))
+GO
 
 -- 3. Que el campo Est_Oco de la tabla Tb_Orden_Compra solo permita
 --los valores 1(Pendiente), 2(Atendida) o 3 (Rechazada)
-
+ALTER TABLE Tb_Orden_Compra
+	ADD CONSTRAINT chk_Est_Oco CHECK
+	(Est_Oco in (1,2,3))
+GO
 
 -- 4. Que el campo Sue_Ven de la tabla Tb_Vendedor
 -- solo permita valores entre 950 y 5000 
 -- de la data actual en la tabla
-
+ALTER TABLE Tb_Vendedor
+	ADD CONSTRAINT chk_Sue_Ven CHECK
+	(Sue_Ven between 980 and 5000)
+GO
 
 --5. Validando que el RUC del cliente (en la tabla Tb_Cliente) para que sean 11 digitos
-
+ALTER TABLE Tb_Cliente
+	ADD CONSTRAINT chk_Ruc_Cli CHECK
+	(Ruc_Cli like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+GO
 
 
 --6. Validando que el RUC del proveedor (en la tabla Tb_Proveedor) para que sean 11 digitos
-
-
+ALTER TABLE Tb_Proveedor
+	ADD CONSTRAINT chk_Ruc_Prv CHECK
+	(Ruc_Prv like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+GO
 --7. Validando que el DNI del vendedor (en la tabla Tb_Vendedor) para que sean 8 digitos
-
+ALTER TABLE Tb_Vendedor
+	ADD CONSTRAINT chk_DNI_Ven CHECK
+	(DNI_Ven like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+GO
 --8. Eliminaremos el contraint de restriccion de sueldos del vendedor (caso 4)
-
+ALTER TABLE Tb_Vendedor
+	DROP CONSTRAINT chk_Sue_Ven
+GO
 --PARTE 2 : CONSTRAINTS DE TIPO DEFAULT (VALORES POR DEFECTO)
 --SINTAXIS DE LOS CONSTRAINT TIPO DEFAULT
 --ALTER TABLE <NOMBRE DE TABLA>
@@ -53,13 +75,21 @@ go
  
 -- 9. Para el caso de no ingresar el campo fec_reg de la tabla Tb_Cliente
 -- asuma para este campo por defecto el valor de la fecha actual 
- 
+ ALTER TABLE Tb_Cliente
+	ADD CONSTRAINT Def_Fec_Reg DEFAULT
+	getdate() FOR Fec_Reg
+GO
  
 
 -- 10. Para el caso de no ingresar el campo Est_prv de la tabla Tb_Proveedor
 -- asuma para este campo por defecto el valor de la fecha actual 
- 
- 
+ALTER TABLE Tb_Proveedor
+	ADD Est_prv datetime NULL
+go
+
+ALTER TABLE Tb_Proveedor
+	ADD DEFAULT getdate() FOR Est_prv
+go
 
 --PARTE 3 : CONSTRAINTS DE UNICIDAD (APLICABLES A CAMPOS NOT NULL QUE DEBEN 
 --ALMACENAR SOLO VALORES UNICOS Y QUE NO SEAN LLAVE PRIMARIA)
@@ -69,11 +99,13 @@ go
  
  -- 11. En la tabla Tb_Vendedor crear un Constraint de Unicidad 
   -- para el DNI del vendedor.
+ALTER TABLE Tb_Vendedor 
+	ADD CONSTRAINT UQ_Dni UNIQUE Dni_ven
 
 
  -- 12. En la tabla Tb_Proveedor crear un Constraint de Unicidad 
  -- para el RUC del proveedor.
-
+ALTER TABLE Tb_Vendedor 
 
  -- 13. En la tabla Tb_Cliente crear un Constraint de Unicidad 
  -- para el RUC del cliente.
