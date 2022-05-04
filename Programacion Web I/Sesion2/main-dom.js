2432bb1713805fb639b3b9762f7401c38df16973
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-05-04 14:51:22 -0500

    fdgh

--- Programacion Web I/Sesion2/main-dom.js
+++ Programacion Web I/Sesion2/main-dom.js
@@ -1,20 +1,99 @@
-const footballTeams = ['Liverpool', 'United', 'Barcelona', 'Real Madrid', 'benfica'];
+const footballTeams = [
+    {
+        name: 'Liverpool',
+        imgUrl: 'https://www.coconis.gr/images/detailed/2/LIVERPOOL_FOOTBALL_ACADEMY2.jpg',
+    },
+    {
+        name: 'Manchested United',
+        imgUrl: 'https://i.pinimg.com/originals/7f/19/a1/7f19a16c3d3409140807a46b5058dbdc.jpg',
+    },
+    {
+        name: 'Barcelona',
+        imgUrl: 'https://image.shutterstock.com/image-vector/flag-barcelona-official-color-background-260nw-1791540650.jpg',
+    },
+    {
+        name: 'Real Madrid',
+        imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKSuGnbWdh-KmcEMe0GCD5MvoyiYeU6_PwuOnAJ4vHjWgNVOnkMBlG3JTgIhjFBuTEllM&usqp=CAU'
+    },
+]
 
 function onDocumentLoaded(){
-    const sectionTest = document.getElementById('test');
+    const sectionTest = document.querySelector('#test');
     console.log(sectionTest);
-    sectionTest.innerHTML = 'Hola DOM';
+    sectionTest.innerHTML = "Hola DOM";
 
-    const listTeams = document.createElement('ul');
+    const listTeams = document.createElement("ul"); //unordered list
+
     footballTeams.forEach((team) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = team;
+        const listItem = document.createElement("li"); //list item
+        listItem.style.fontSize = '30px';
+        
+        const txt = document.createTextNode(team.name);
+
+        const img = document.createElement('img');
+        img.src = team.imgUrl;
+        img.style.height = '30px';
+        img.style.marginLeft = '20px';
+        img.style.cursor='pointer';
+        img.addEventListener('click', function(){
+            alert(team.name);
+        })
+        /*img.addEventListener('mousemove', function(event){
+            console.log({
+                x: event.offsetX,
+                y: event.offsetY,
+            })
+        })*/
+
+        const link = document.createElement('a');
+        link.href = 'https://www.google.com/search?q='+ team.name +'+footbal+team';
+        link.innerHTML = 'ver más';
+        link.target = '_blank';
+        link.style.marginLeft = '20px';
+        link.style.textDecoration = 'none';
+
+        listItem.appendChild(txt);
+        listItem.appendChild(img);
+        listItem.appendChild(link);
 
         listTeams.appendChild(listItem);
     })
 
     console.log(listTeams);
+
     sectionTest.appendChild(listTeams);
+
+    const voltorb = document.createElement('img');
+    voltorb.src = 'https://www.pngplay.com/wp-content/uploads/12/Voltorb-Pokemon-PNG-Clipart-Background.png';
+    voltorb.style.height = '60px';
+    document.body.appendChild(voltorb);
+
+    document.addEventListener('mousemove', function(event){
+        voltorb.style.position = 'absolute';
+        voltorb.style.left = event.pageX + 'px';
+        voltorb.style.top = event.pageY + 'px';
+    })
 }
 
-window.onload = onDocumentLoaded;
+/*
+<ul>
+    <li>Liverpool</li>
+    <li>United</li>
+    <li>Barcelona</li>
+    *****
+</ul>
+*/
+
+//Decimos a window que al momento que todo su contenido haya cargado en el navegador
+//ejecute la función definida como onload
+//window.onload = onDocumentLoaded;
+//window.addEventListener('load', onDocumentLoaded);
+
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);
+
+
+
+
+
+
+
--- Programacion Web I/Sesion2/main.js
+++ Programacion Web I/Sesion2/main.js
@@ -1,3 +1,44 @@
-const sectionTest = $('$#test');
-console.log(sectionTest);
 
+const footballTeams = [
+    {
+        name: 'Liverpool',
+        imgUrl: 'https://www.coconis.gr/images/detailed/2/LIVERPOOL_FOOTBALL_ACADEMY2.jpg',
+    },
+    {
+        name: 'Manchested United',
+        imgUrl: 'https://i.pinimg.com/originals/7f/19/a1/7f19a16c3d3409140807a46b5058dbdc.jpg',
+    },
+    {
+        name: 'Barcelona',
+        imgUrl: 'https://image.shutterstock.com/image-vector/flag-barcelona-official-color-background-260nw-1791540650.jpg',
+    },
+    {
+        name: 'Real Madrid',
+        imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKSuGnbWdh-KmcEMe0GCD5MvoyiYeU6_PwuOnAJ4vHjWgNVOnkMBlG3JTgIhjFBuTEllM&usqp=CAU'
+    },
+]
+
+
+//Obtengo el section con id test de html
+const sectionTest = $('#test');
+sectionTest.css('font-size', '30px');
+
+const h1 = $('<h1></h1>');
+h1.text('jQuery Example');
+
+const ul = $('<ul></ul>');
+footballTeams.forEach(team => {
+    const li = $('<li></li>');
+    li.append(`
+        ${team.name}
+        <img style='height: 30px; margin-left: 20px' src='${team.imgUrl}'/>
+        <a href='${'https://www.google.com/search?q='+ team.name +'+footbal+team'}'>Ver más</a>
+    `);
+
+    ul.append(li);
+})
+
+sectionTest.append(h1);
+sectionTest.append(ul);
+
+//Crear una table y mostrar los datos del arreglo en ella.
