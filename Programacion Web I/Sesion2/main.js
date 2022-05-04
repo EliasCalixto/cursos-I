bee8668c2a54e9703b4322da35ce17036121d2c2
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-05-04 09:07:03 -0500

    fgf

--- Programacion Web I/Sesion2/main.js
+++ Programacion Web I/Sesion2/main.js
@@ -1,20 +1,3 @@
-const footballTeams = ['Liverpool', 'United', 'Barcelona', 'Real Madrid', 'benfica'];
-
-function onDocumentLoaded(){
-    const sectionTest = document.getElementById('test');
-    console.log(sectionTest);
-    sectionTest.innerHTML = 'Hola DOM';
-
-    const listTeams = document.createElement('ul');
-    footballTeams.forEach((team) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = team;
-
-        listTeams.appendChild(listItem);
-    })
-
-    console.log(listTeams);
-    sectionTest.appendChild(listTeams);
-}
-
-window.onload = onDocumentLoaded;
+const {JSDOM} = require( "jsdom");
+const {window} = new JSDOM( "" );
+const $ = require( "jquery" )( window );
