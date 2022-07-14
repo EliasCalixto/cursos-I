ea17e98b62ec6baad8924114f26299d11a0cd713
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-07-13 22:21:12 -0500

    w

---
+++ Programacion Web I/EP4/Front-end/fetchData.js
@@ -0,0 +1,28 @@
+const fetchData = (url) => {
+    return new Promise(async (resolve, reject) => {
+        try{
+            const data = await fetch(url);
+            const json = await data.json();
+            resolve(json);
+        } catch(error){
+            reject(error);
+        }
+    });
+}
+
+export const postData = (url,data) => {
+    return new Promise(async (resolve,reject) =>{
+        try{
+            const dataPost = await fetch(url,{
+                method:'POST',
+                body: data,
+            });
+            const json = await dataPost.json();
+            resolve(json);
+        }catch(error){
+            reject(error);
+        }
+    });
+}
+
+export default fetchData;
---
+++ Programacion Web I/EP4/Front-end/main-web-services.js
@@ -0,0 +1,78 @@
+import fetchData from "./fetchData.js";
+
+const API_URL = "http://localhost/back-end/";
+const ENDPOINT_TASKS= "tareas.php";
+
+let tasks=[];
+
+const fetchTasks = async () =>{
+    try{
+        const resultTasks = await fetchData(`${API_URL}${ENDPOINT_TASKS}`);
+        tasks = resultTasks.tasks;
+        console.log(tasks)
+    }catch(err){
+        console.log(err);
+    }
+}
+
+window.onload = async ()=> {
+    try{
+        await fetchTasks();
+
+        const tasksSection = document.querySelector('#tasks-section');
+
+        let htmlTable = 
+        `<table class='table table-striped'>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>createdAt</th>
+                </tr>
+            </head>
+                <tbody>`;
+        tasks.forEach(task => {
+            htmlTable +=
+            `<tr>
+                <td>${task.id}</td>
+                <td>${task.name}</td>
+                <td>${task.createdAt}</td>
+                <td><button type=button class=border><img class='check' src='check.png'></button></td>
+                <td><button type=button class=border>Editar</button></td>
+                <td><button type=button class=border>Eliminar</button></td>
+            </tr>
+            `
+        })
+        htmlTable+='</tbody></table>';
+
+        tasksSection.innerHTML = htmlTable;
+
+        const tasksSection2 = document.querySelector('#tasks-section2');
+
+        let htmlTable2 = 
+        `<table class='table table-striped'>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>createdAt</th>
+                </tr>
+            </head>
+                <tbody>`;
+        tasks.forEach(task => {
+            htmlTable2 +=
+            `<tr>
+                <td></td>
+                <td>'Todavia no hay tareas realizadas.</td>
+                <td></td>
+                <td><button type=button class=border>Eliminar</button></td>
+            </tr>
+            `
+        })
+        htmlTable2+='</tbody></table>';
+
+        tasksSection2.innerHTML = htmlTable2;
+    }catch(err){
+        console.log(err);
+    }
+}
